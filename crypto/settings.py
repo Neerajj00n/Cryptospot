3c0a5639ec2bf5285c15b61bbec345870a4c02b6
Neeraj joon <34393059+neerajjoon007@users.noreply.github.com>
2019-03-01 18:04:08 +0530

    Update settings.py

--- crypto/settings.py
+++ crypto/settings.py
@@ -156,8 +156,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'webslayer01@gmail.com'
-EMAIL_HOST_PASSWORD = '@Joon007'
+EMAIL_HOST_USER = #Your Email
+EMAIL_HOST_PASSWORD = #password
 
 
 
